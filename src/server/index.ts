ce212ac545d62aac3f5616f1bcca121393618569
Douglas Oliveira <douglaspo_97@outlook.com>
2025-02-17 12:43:13 -0300

    feat: create route adapater to abstract controller input and output

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,29 +1,13 @@
 import express from 'express';
+import { routeAdapter } from './adapters/routeAdapter';
 import { makeSignUpController } from '../factories/makeSignUpController';
 import { makeSignInController } from '../factories/makeSignInController';
 
 const app = express();
 app.use(express.json());
 
-app.post('/sign-up', async (req, res) => {
-  const signUpController = makeSignUpController();
-  
-  const { statusCode, body } = await signUpController.handle({
-    body: req.body
-  });
-
-  res.status(statusCode).json(body);
-});
-
-app.post('/sign-in', async (req, res) => {
-  const signInController = makeSignInController();
-
-  const { statusCode, body } = await signInController.handle({
-    body: req.body
-  });
-
-  res.status(statusCode).json(body);
-});
+app.post('/sign-up', routeAdapter(makeSignUpController()));
+app.post('/sign-in', routeAdapter(makeSignInController()));
 
 app.listen(3001, () => {
   console.log('🚀 Server started at http://localhost:3001');
