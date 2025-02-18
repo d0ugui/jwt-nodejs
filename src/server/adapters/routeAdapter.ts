fa896e1b2f42a251fc258b4ebbc149ed09025f7f
Douglas Oliveira <douglaspo_97@outlook.com>
2025-02-18 19:44:03 -0300

    feat: create authentication middleware to validate the request token sended in headers

--- src/server/adapters/routeAdapter.ts
+++ src/server/adapters/routeAdapter.ts
@@ -4,7 +4,8 @@ import { IController } from '../../application/interfaces/IController';
 export function routeAdapter(controller: IController) {
   return async (req: Request, res: Response) => {
     const { statusCode, body } = await controller.handle({
-      body: req.body
+      body: req.body,
+      accountId: req.metadata?.accountId,
     });
     
     res.status(statusCode).json(body);
