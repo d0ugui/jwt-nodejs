89e9367185cc87b57c8f15422c2de131cd8d9b83
Douglas Oliveira <douglaspo_97@outlook.com>
2025-02-18 19:44:47 -0300

    chore: set correctly version of @types/express and create type environments

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,12 +2,18 @@ import express from 'express';
 import { routeAdapter } from './adapters/routeAdapter';
 import { makeSignUpController } from '../factories/makeSignUpController';
 import { makeSignInController } from '../factories/makeSignInController';
+import { middlewareAdapter } from './adapters/middlewareAdapter';
+import { makeAuthenticationMiddleware } from '../factories/makeAuthenticationMiddleware';
+import { makeGetUserController } from '../factories/makeGetUserController';
 
 const app = express();
+
 app.use(express.json());
 
 app.post('/sign-up', routeAdapter(makeSignUpController()));
 app.post('/sign-in', routeAdapter(makeSignInController()));
+
+app.get('/me', middlewareAdapter(makeAuthenticationMiddleware()), routeAdapter(makeGetUserController()));
 
 app.listen(3001, () => {
   console.log('🚀 Server started at http://localhost:3001');
