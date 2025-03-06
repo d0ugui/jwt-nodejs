0f8498aeec559caa5cd0231c88dd6fddb4dd98aa
Douglas Oliveira <douglaspo_97@outlook.com>
2025-03-06 19:52:15 -0300

    test: unit tests in get-user-controller to validate account-id type data

--- src/application/controllers/GetUserController.spec.ts
+++ src/application/controllers/GetUserController.spec.ts
@@ -19,4 +19,16 @@ describe('get-user controller tests', () => {
 
     await expect(handle).rejects.toThrow(new Error('Unexpected error'));
   });
+
+  test('should throw zod error when get a invalid account-id type', async () => {
+    const getUserController = new GetUserController(mockedGetUserUseCase);
+
+    const handle = await getUserController.handle({ 
+      body: { name: 'Douglas', email: 'd@d.com', password: '12345678' }, 
+      accountId: 123 as unknown as string
+    });
+
+    expect(handle.statusCode).toBe(400);
+    expect(handle.body?.[0]?.code).toBe('invalid_type');
+  });
 });
