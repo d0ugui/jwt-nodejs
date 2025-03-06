0c0194ae65a9ff09870aa7e3e9c3d7ee1662d34c
Douglas Oliveira <douglaspo_97@outlook.com>
2025-03-05 21:24:22 -0300

    test: write some tests cases to sign-up-controller

--- src/application/controllers/SignUpController.e2e-spec.ts
+++ src/application/controllers/SignUpController.e2e-spec.ts
@@ -22,4 +22,38 @@ describe('sign-up (e2e)', () =>{
 
     expect(account).toBeTruthy();
   });
+
+  it('should throw zod-error when email has invalid format', async () => {
+    const response = await request(app)
+      .post('/sign-up')
+      .send({
+        name: 'John Doe',
+        email: 'john.doe.com',
+        password: '12345678'
+      });
+  
+    expect(response.statusCode).toBe(400);
+    expect(response.body[0].code).toBe('invalid_string');
+  });
+
+  it('should throw account-already-exists when email is already in use', async () => {
+    await request(app)
+      .post('/sign-up')
+      .send({
+        name: 'John Doe 1',
+        email: 'john@doe1.com',
+        password: '12345678'
+      });
+
+    const response = await request(app)
+      .post('/sign-up')
+      .send({
+        name: 'John Doe 1',
+        email: 'john@doe1.com',
+        password: '12345678'
+      });
+  
+    expect(response.statusCode).toBe(409);
+    expect(response.body.error).toBe('This email is already in use.');
+  });
 });
